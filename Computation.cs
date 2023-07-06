9bd2d28642bfa0483031470d8e61cddd066fc475
VictorMauroy <mauroy.victor@gmail.com>
2023-07-06 18:25:51 +0200

    feat: :sparkles: Create a basic function to divide numbers and generate Unit Test for it.

---
+++ Computation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_CSharp
+{
+    public static class Computation
+    {
+        /// <summary>
+        /// Divide two integer numbers : a dividend and a divisor, then return the result.
+        /// </summary>
+        /// <param name="dividend">Dividend of the division</param>
+        /// <param name="divisor">Divisor of the division</param>
+        /// <returns>Quotient of the division</returns>
+        public static float GetDivision(int dividend, int divisor)
+        {
+            return dividend / (float)divisor;
+            // Note: Convert divisor as float to avoid getting a rounded integer
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Testing_CSharp;
+
+float quotient = Computation.GetDivision(19, 13);
+Console.WriteLine(quotient);
