290f81c8e622b6b13179230ee5eb0fe16a4c6192
VictorMauroy <mauroy.victor@gmail.com>
2023-07-07 10:18:05 +0200

    feat: :sparkles: Create class Cat that defines an object with properties.

---
+++ Cat.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_CSharp
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+        /// <summary>
+        /// Age in years
+        /// </summary>
+        public int Age { get; set; } 
+        public float Weigth { get; set; }
+
+        /// <summary>
+        /// Constructor of the Cat class
+        /// </summary>
+        /// <param name="name">Name of your cat</param>
+        /// <param name="weigth">Current weigth of your cat</param>
+        public Cat(string name, int age, float weigth) 
+        {
+            Name = name;
+            Age = age;
+            Weigth = weigth;
+        }
+        
+        /// <summary>
+        /// Add weigth in kg to your cat.
+        /// </summary>
+        /// <param name="weigthToAdd">Weigth in kg to add</param>
+        /// <returns>new weigth of your cat</returns>
+        public float RaiseWeigth(float weigthToAdd)
+        {
+            Weigth+= weigthToAdd;
+            return Weigth;
+        }
+
+        public int UpdateAge(int newAge)
+        {
+            Age = newAge;
+            return Age;
+        }
+        
+        public string GetCatProfile()
+        {
+            return Name + ": " + Age + " years old has a weigth of " + Weigth + "kg.";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using Testing_CSharp;
+﻿using System.Xml.Linq;
+using Testing_CSharp;
 
 float quotient = Computation.GetDivision(19, 13);
 Console.WriteLine(quotient);
+
+Cat kitty = new Cat("Kitty", 1, 1.5f);
+kitty.RaiseWeigth(0.5f);
+kitty.UpdateAge(2);
+
+Console.WriteLine(kitty.GetCatProfile());
